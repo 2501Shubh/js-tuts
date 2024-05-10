2811db51cec21a9e77e531a548ee7b5f29296b92
2501Shubh <153253024+2501Shubh@users.noreply.github.com>
2024-05-10 04:14:00 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";   //treat all Js code as a newer version
+
+//alert(3+3)    -> we are using nodejs, not browser. 
+//                 therefore, different syntax for using it.
+
+console.log("Shubh")
+
+console.log(3
+    +3)     
+    //can be written like this but not preferable as code readability should be high!
+
+let name = "Shubhrangini"       //string
+let age = 22                    //number
+let isLoggedIn = true           //boolean
+let state = null                //null
+let city                        //undefined
+
+/* Primitive Datatypes */
+
+//number -> range: 2^53
+//bigint -> for larger numbers
+//string -> "assembly of characters"
+//boolean -> true or false
+
+/*
+    null -> standalone value(if it is assigned to a variable, it means that the variable is empty(or currently, it has no value))
+    type: object
+*/
+
+/*
+    undefined -> placeholder for a value which is not defined yet
+    type: undefined
+*/
