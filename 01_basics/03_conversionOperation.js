8d52cda3006c84db02b226629129cf0af53c9a4f
2501Shubh <153253024+2501Shubh@users.noreply.github.com>
2024-05-11 03:59:37 +0000

    conversion and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,6 +29,36 @@ let convertLoggedIn = Boolean(isLoggedIn)
 // console.log(typeof convertLoggedIn);
 // console.log(convertLoggedIn);
 
+//convert to Number
+
+let isPresent = true
+let checkIsPresent = Number(isPresent)
+
+console.log(typeof checkIsPresent)
+console.log(checkIsPresent)
+
+let a = null
+console.log(typeof a);  //type: object
+
+let convertA = Number(a)
+
+console.log(typeof convertA)
+console.log(convertA)   //null means empty but due to conversion it was made 0, which is wrong
+
+/*
+    -> we are asked to return the temp
+    -> temp is null(this means we dont have the temperature deets)
+    -> in that case, if we return 0, it will be wrong!
+*/
+
+let b = undefined
+console.log(typeof b)   //type: undefined
+
+let convertB = Number(b)
+
+console.log(typeof convertB);
+console.log(b);
+
 
 //  ****** OPERATIONS ******
 
@@ -46,17 +76,19 @@ let convertLoggedIn = Boolean(isLoggedIn)
 // console.log(2 + 1 + "2");    -> 32(as number first)
 // console.log("2" + 1 + 2);    -> 212(as string first)
 
+console.log(+true);     //another way to convert into Number
+
 
 //  ****** PREFIX & POSTFIX ******
 
 //PREFIX
-let x = 3
-let y = x++     //value is not incremented here, first use then update
-console.log(x);
-console.log(y);
+// let x = 3
+// let y = x++     -> value is not incremented here, first use then update
+// console.log(x);
+// console.log(y);
 
 //POSTFIX
-let a = 3
-let b = ++a     //first update(increment), then use
-console.log(a);
-console.log(b);
+// let a = 3
+// let b = ++a    ->  first update(increment), then use
+// console.log(a);
+// console.log(b);
